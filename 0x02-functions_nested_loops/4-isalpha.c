fa1f0e5125ff07754024b72e0a8151d0cc2ebe88
Obafemitobi438 <obafemitobi438@gmail.com>
2022-12-14 01:39:03 -0800

    o

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _isalpha - checks for char alphabet
+* -isalpha - checks for char alphabet
 * @c: the char to be checked
 * Return: 1 if char is the letter lower or upper, 0 otherwise
 **/
 
-int _isalpha(int c)
+int -isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
 		return (1);
