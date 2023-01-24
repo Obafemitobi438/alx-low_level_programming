8d4671ef4ee55346829356c8adfb85fa29230980
Obafemitobi438 <obafemitobi438@gmail.com>
2023-01-24 09:21:18 -0800

    1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,11 +8,22 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') ||
-	(c >= 'A' && <= 'Z'))
+	if (c >= 'a' && c <= 'z')
+
+	{
 		return (1);
 
+	}
+
+	else if (c >= 'A' && c <= 'Z')
+
+	{
+		return (1);
+
+	}
+
 	else
+	{
 		return (0);
 
-}
+	}
