a025152460690dd92b7c1aa26a5a2a23a2caad8f
Obafemitobi438 <obafemitobi438@gmail.com>
2023-01-24 10:54:57 -0800

    2

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,47 +1,54 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-#include <stdlib.h>
-
-#include <udis86.h>
-
 /**
- * main - Prints opcodes of its own main function
- * @argc: The number of arguments in the command line
- * @argv: The array of strings
- * Return: Opcodes
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ * Return: void
  **/
 
-int main(int argc, char *argv[])
+void print_opcodes(char *a, int n)
 
 {
-	ud_t ud_obj;
+	int i;
 
-	int val = 0, i = 0;
+	for (i = 0; i < n; i++)
 
-	if (argc == 2)
 	{
-		val = atoi(argv[]);
 
-		if (val < 0)
-		{
-			printf("Error\n");
-
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-
-		ud_set_mode(&ud_obj, 64);
-
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-
-		}
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
 	}
+	printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ * Return: always O
+ **/
+
+int main(int argc, char **argv)
+
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
 	return (0);
 }
