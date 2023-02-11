57b84b4c45d7d6b59d48ffd01c835f295e5d439e
Obafemitobi438 <obafemitobi438@gmail.com>
2023-02-10 18:09:09 -0800

    hg

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,37 +1,20 @@
 #include <elf.h>
-
 #include <sys/types.h>
-
 #include <sys/stat.h>
-
 #include <fcntl.h>
-
 #include <unistd.h>
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 void check_elf(unsigned char *e_ident);
-
 void print_magic(unsigned char *e_ident);
-
 void print_class(unsigned char *e_ident);
-
 void print_data(unsigned char *e_ident);
-
 void print_version(unsigned char *e_ident);
-
 void print_abi(unsigned char *e_ident);
-
 void print_osabi(unsigned char *e_ident);
-
 void print_type(unsigned int e_type, unsigned char *e_ident);
-
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-
 void close_elf(int elf);
 
 /**
@@ -332,6 +315,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
+
 	free(header);
 	close_elf(o);
 	return (0);
