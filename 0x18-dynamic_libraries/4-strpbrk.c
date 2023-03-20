c16c0894e72c351a4fac113ef5fa96de67dfc4c5
Obafemitobi438 <obafemitobi438@gmail.com>
2023-03-20 10:47:57 -0700

    j

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
  * @s:first occurrence in the string
  * @accept: matches one of the bytes, or @NULL if no such byte
- * Return: a pointer to the byte
+ * Return: NULL
  **/
 
 char *_strpbrk(char *s, char *accept)
